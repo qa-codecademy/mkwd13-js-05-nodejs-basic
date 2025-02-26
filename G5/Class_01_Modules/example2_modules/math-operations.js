2efb330747191f9195b68be5cf8d4399a5961322
Ivo Kostovski <ivo.kostovski@gmail.com>
2025-02-26 20:49:05 +0100

    class 1 done

---
+++ G5/Class_01_Modules/example2_modules/math-operations.js
@@ -0,0 +1,11 @@
+export const add = (a, b) => a + b;
+
+export const subtract = (a, b) => a - b;
+
+// Naming conventions
+// kebap-case
+// PascalCase
+// camelCase
+// snake_case
+
+console.log(add(1, 2));
