ae00617fa28e9471ad1f607a568f5726862e07c5
Ivo Kostovski <ivo.kostovski@gmail.com>
2025-02-28 20:49:43 +0100

    class 2 done

--- G5/Class_02_Nodejs_Modules/example2_mini-app/index.js
+++ G5/Class_02_Nodejs_Modules/example2_mini-app/index.js
@@ -1,4 +1,28 @@
-function main() {}
+import {
+	createTask,
+	readAllTasks,
+	updateTask,
+	deleteTask,
+} from './task-manager.js';
+
+function main() {
+	// createTask('Do homework 2', 'Homework 2, Nodejs Basic');
+	// createTask('Do homework 3', 'Homework 3, Nodejs Basic');
+	// createTask('Do homework 4', 'Homework 4, Nodejs Basic');
+	// createTask('Do homework 5', 'Homework 5, Nodejs Basic');
+
+	// updateTask(
+	// 	'324112412',
+	// 	'Nov title',
+	// 	'Nov description'
+	// );
+
+	console.log('All tasks:', readAllTasks());
+
+	deleteTask('b3e1743e-5820-4dd6-92ec-4e07f5e5fd52');
+
+	console.log('All tasks:', readAllTasks());
+}
 
 main();
 
--- G5/Class_02_Nodejs_Modules/example2_mini-app/task-manager.js
+++ G5/Class_02_Nodejs_Modules/example2_mini-app/task-manager.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
+import { v4 as uuidv4 } from 'uuid';
 
 // Fix __dirname and __filename
 const ___filename = fileURLToPath(import.meta.url);
@@ -10,11 +11,57 @@ const __dirname = path.dirname(___filename);
 const tasksFile = path.join(__dirname, 'data', 'tasks.json'); // Path to the Database
 
 // Read all tasks from the database
+export function readAllTasks() {
+	const tasks = fs.readFileSync(tasksFile);
+	const parsedTasks = JSON.parse(tasks);
+	return parsedTasks;
+}
 
 // Get a single task
 
 // Create a new task
+export function createTask(title, description) {
+	const tasks = readAllTasks();
+
+	const newTask = {
+		id: uuidv4(),
+		title,
+		description,
+		completed: false,
+		createdAt: new Date().toISOString(), // 12/12/2025
+	};
+
+	tasks.push(newTask);
+
+	fs.writeFileSync(tasksFile, JSON.stringify(tasks, null, 2));
+}
 
 // Update a task
+export function updateTask(id, title, description) {
+	const tasks = readAllTasks();
+
+	// find the task
+	const taskIndex = tasks.findIndex(task => task.id === id);
+
+	if (taskIndex < 0) {
+		throw new Error(`Task with ${id} not found.`);
+	}
+
+	// update the task
+	tasks[taskIndex].title = title;
+	tasks[taskIndex].description = description;
+
+	// save it back
+	fs.writeFileSync(tasksFile, JSON.stringify(tasks, null, 2));
+}
 
 // Delete a task
+export function deleteTask(id) {
+	// read all tasks
+	const tasks = readAllTasks();
+
+	const filteredTasks = tasks.filter(task => task.id !== id);
+
+	// save all tasks back to DB
+	fs.writeFileSync(tasksFile, JSON.stringify(filteredTasks, null, 2));
+}
