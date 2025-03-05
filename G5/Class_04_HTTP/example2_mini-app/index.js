b82efe4839824e78942b2005f638331de4fd9564
Ivo Kostovski <ivo.kostovski@gmail.com>
2025-03-05 20:59:05 +0100

    html done

--- G5/Class_04_HTTP/example2_mini-app/index.js
+++ G5/Class_04_HTTP/example2_mini-app/index.js
@@ -15,6 +15,10 @@ const server = http.createServer((req, res) => {
 	const url = req.url;
 	const method = req.method;
 
+	console.log(
+		`Server has been called with method: ${method} on: http://${HOSTNAME}:${PORT}${url}`
+	);
+
 	if (url === '/' || url === '/home') {
 		res.writeHead(200);
 		res.end(
@@ -22,9 +26,51 @@ const server = http.createServer((req, res) => {
 				message: 'Welcome to our books API!',
 			})
 		);
-	} else if (url === '/books') {
-		res.writeHead(200);
-		res.end(JSON.stringify(books));
+	} else if (url.startsWith('/books')) {
+		if (method === 'GET') {
+			const id = url.split('/')[2]; // /books/1 => ['', 'book', '1',]
+
+			if (id) {
+				// Case 1: Get a single book by ID
+				const book = books.find(book => book.id === parseInt(id));
+				res.writeHead(200);
+				res.end(JSON.stringify(book));
+			} else {
+				// Case 2: Get all books
+				res.writeHead(200);
+				res.end(JSON.stringify(books));
+			}
+		} else if (method === 'POST') {
+			// Get the body from the request
+			let body = ``;
+
+			req.on('data', chunk => {
+				body += chunk.toString();
+			});
+
+			req.on('end', () => {
+				const parsedBody = JSON.parse(body);
+				console.log('parsedBody', parsedBody);
+
+				// Add the new book
+				const newBook = {
+					...parsedBody,
+					id: books.length + 1,
+				};
+				books.push(newBook);
+
+				// Response
+				res.writeHead(201);
+				res.end(JSON.stringify(newBook));
+			});
+		} else {
+			res.writeHead(405);
+			res.end(
+				JSON.stringify({
+					error: 'Method not allowed',
+				})
+			);
+		}
 	} else {
 		// Catch all route / endpoint
 		res.writeHead(404);
