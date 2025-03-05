3b0fc70b06a8e6b39e70978e847f36ed79879ae2
Ivo Kostovski <ivo.kostovski@gmail.com>
2025-03-05 19:43:49 +0100

    example 2 init

--- G5/Class_04_HTTP/example1_basic/index.js
+++ G5/Class_04_HTTP/example1_basic/index.js
@@ -3,9 +3,31 @@ import http from 'http';
 // request > req
 // response > res
 
+const PORT = 3000; // default PORT for servers
+const HOSTNAME = 'localhost'; // physical IP for every PC 126.0.0.1 // 86.123.123.01
+
 const server = http.createServer((request, response) => {
 	// console.log('Request', request);
 	// console.log('Response', response);
+
+	console.log(
+		`Server has been called on: ${request.method} http://${HOSTNAME}:${PORT}${request.url}`
+	);
+
+	if (request.url === '/users') {
+		response.writeHead(200);
+		response.end('Here are the users for you...');
+	} else if (request.url === '/students') {
+		response.writeHead(200);
+		response.end('Here are the students for you...');
+	} else if (request.url === '/') {
+		response.writeHead(200);
+		response.end('Welcome to the Homepage!');
+	} else {
+		response.writeHead(404);
+		response.end('Not found');
+	}
+
 	// We can send back textual responses
 	// response.writeHead(200, { // writeHead method is used to send status and add headers
 	// 	'Content-Type': 'text/plain',
@@ -22,9 +44,6 @@ const server = http.createServer((request, response) => {
 	//   </div>
 	//   `);
 });
-
-const PORT = 3000; // default PORT for servers
-const HOSTNAME = 'localhost'; // physical IP for every PC 126.0.0.1 // 86.123.123.01
 
 server.listen(PORT, HOSTNAME, () => {
 	// Callback is called when server is live
