580681732520ac9f6d289b71433938978b0d73af
Ivo Kostovski <ivo.kostovski@gmail.com>
2025-03-05 18:52:33 +0100

    example 1 + Readme

---
+++ G5/Class_04_HTTP/example1_basic/index.js
@@ -0,0 +1,32 @@
+import http from 'http';
+
+// request > req
+// response > res
+
+const server = http.createServer((request, response) => {
+	// console.log('Request', request);
+	// console.log('Response', response);
+	// We can send back textual responses
+	// response.writeHead(200, { // writeHead method is used to send status and add headers
+	// 	'Content-Type': 'text/plain',
+	// });
+	// response.end('Successful request and response'); // end method is used to send the response and to add content
+	// We can send back HTML responses
+	// response.writeHead(200, { // writeHead method is used to send status and add headers
+	// 	'content-type': 'text/html',
+	// });
+	// response.end(` // end method is used to send the response and to add content
+	//   <div>
+	//     <h1>Title</h1>
+	//     <p>This was amazing</p>
+	//   </div>
+	//   `);
+});
+
+const PORT = 3000; // default PORT for servers
+const HOSTNAME = 'localhost'; // physical IP for every PC 126.0.0.1 // 86.123.123.01
+
+server.listen(PORT, HOSTNAME, () => {
+	// Callback is called when server is live
+	console.log(`Server started listening at http://${HOSTNAME}:${PORT}`);
+});
