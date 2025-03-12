b0b22c6831e4bcec727f758277a6234927be7b2e
anetastankovska <anetastankovskaane@gmail.com>
2025-03-12 18:43:09 +0100

    wip class code

--- G1/Class05-Express/code/server/services/students.service.js
+++ G1/Class05-Express/code/server/services/students.service.js
@@ -57,6 +57,7 @@ const addStudent = (newStudentData) => {
   return newStudent;
 };
 
+// 4. Get student by ID
 const getStudentById = (studentId) => {
   const students = getStudents();
   const foundStudent = students.find((student) => student.id === studentId);
@@ -64,14 +65,44 @@ const getStudentById = (studentId) => {
   return foundStudent;
 };
 
+// 5. Delete student by ID
 const deleteStudent = (studentId) => {
   const students = getStudents();
   const updatedStudents = students.filter(
     (student) => student.id !== studentId
   );
+  console.log(students.length, updatedStudents.length);
   if (students.length === updatedStudents.length) {
     throw new Error("Student to delete not found!");
   }
+  saveStudentData(updatedStudents);
 };
 
-export { getStudents, getStudentById, addStudent, deleteStudent };
+// 6. Update student by ID
+const updateStudent = (studentId, studentUpdateData) => {
+  const students = getStudents();
+  const foundStudentIndex = students.findIndex(
+    (student) => student.id === studentId
+  );
+  console.log(foundStudentIndex);
+
+  // If student is not found
+  if (foundStudentIndex < 0) throw new Error("Student not found");
+
+  const updatedStudentData = {
+    ...students[foundStudentIndex],
+    ...studentUpdateData,
+  };
+
+  students[foundStudentIndex] = updatedStudentData;
+  saveStudentData(students);
+  return updatedStudentData;
+};
+
+export {
+  getStudents,
+  getStudentById,
+  addStudent,
+  deleteStudent,
+  updateStudent,
+};
