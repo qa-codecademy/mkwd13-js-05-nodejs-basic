57827babeb78840f5c20028426f1dc780ad33506
Ivo Kostovski <ivo.kostovski@gmail.com>
2025-03-13 07:57:13 +0100

    add MVC with FE

--- G5/Class_06_MVC/example1_mini-app/index.js
+++ G5/Class_06_MVC/example1_mini-app/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import userRouter from './routes/user.routes.js';
 import logger from './middlewares/logger.middleware.js';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Defining consts
 const PORT = 3000;
@@ -11,10 +15,11 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.static(join(__dirname, 'public')));
 app.use(logger);
 
-// Routes
-app.use('/users', userRouter);
+// API Routes
+app.use('/api/users', userRouter);
 
 // Starting the server
 app.listen(PORT, HOSTNAME, () => {
