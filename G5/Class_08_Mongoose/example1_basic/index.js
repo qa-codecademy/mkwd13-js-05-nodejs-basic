14427d7d26e00263d50055caf7e6b3ea9843ef84
Ivo Kostovski <ivo.kostovski@gmail.com>
2025-03-19 19:41:32 +0100

    Add envs

--- G5/Class_08_Mongoose/example1_basic/index.js
+++ G5/Class_08_Mongoose/example1_basic/index.js
@@ -1,48 +1,60 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import Customer from './models/customer.model.js';
+import dotenv from 'dotenv';
 
-const PORT = 3000;
-const HOSTNAME = 'localhost';
-const MONGO_USERNAME = 'ivokostovski';
-const MONGO_PASSWORD = 'V1qwGccfdoAu3y8X';
-const MONGO_CLUSTER = 'cluster0.3y9yiup';
-const MONGO_DB_NAME = 'repair-shop';
+dotenv.config();
 
-const URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CLUSTER}.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+const {
+	MONGO_CLUSTER,
+	MONGO_DB_NAME,
+	MONGO_PASSWORD,
+	MONGO_USERNAME,
+	PORT,
+	HOSTNAME,
+} = process.env;
 
 const app = express();
-
-mongoose.connect(URI).catch(error => {
-	console.error(error);
-});
 
 // Middleware
 app.use(express.json());
 
 // Endpoints
-app.get('/api/customers', (req, res) => {
-	// TODO: Get customers from the DB
-	res.send([]);
+app.get('/api/customers', async (req, res) => {
+	const customers = await Customer.find();
+
+	res.send(customers);
 });
 
 app.post('/api/customers', async (req, res) => {
-	const body = req.body;
-	// TODO: Create a new custom in the DB
-	const customer = new Customer({
-		name: body.name,
-		email: body.email,
-		location: body.location,
-		age: body.age,
-		phone: body.phone,
+	try {
+		const body = req.body;
+
+		const customer = new Customer({
+			name: body.name,
+			email: body.email,
+			location: body.location,
+			age: body.age,
+			phone: body.phone,
+		});
+
+		const createdCustomer = await customer.save();
+
+		// Return the created customer
+		res.status(201).send(createdCustomer);
+	} catch (error) {
+		res.status(500).send({ message: error.message });
+	}
+});
+
+async function init() {
+	const URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CLUSTER}.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+
+	await mongoose.connect(URI);
+
+	app.listen(PORT, HOSTNAME, () => {
+		console.log(`Server started listening on http://${HOSTNAME}:${PORT}`);
 	});
+}
 
-	const createdCustomer = await customer.save();
-
-	// Return the created customer
-	res.send(createdCustomer);
-});
-
-app.listen(PORT, HOSTNAME, () => {
-	console.log(`Server started listening on http://${HOSTNAME}:${PORT}`);
-});
+init();
