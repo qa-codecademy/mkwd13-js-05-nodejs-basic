d2371e82084e823c09913f4374d8cd052f02a9f3
anetastankovska <anetastankovskaane@gmail.com>
2025-03-19 18:36:48 +0100

    wip class 08 code

--- G1/Class07-MongoDB/my-movies-app/controlers/movie.controller.js
+++ G1/Class07-MongoDB/my-movies-app/controlers/movie.controller.js
@@ -25,4 +25,91 @@ export default class MovieController {
       res.status(500).json({ error: error.message });
     }
   }
+
+  async createMovie(req, res) {
+    try {
+      const { name, description, genre, director, year } = req.body;
+      const movieData = {
+        name,
+        director,
+        description,
+        genre,
+        year,
+        createdAt: new Date(),
+      };
+      const id = await this.movieService.create(movieData);
+      res.status(201).json({ id, ...movieData });
+      // const movie = await this.movieService.create(movieData);
+      // res.status(201).json(movie)
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  async updateMovie(req, res) {
+    try {
+      const { name, description, genre, director, year } = req.body;
+
+      const movieData = {
+        ...(name && { name }),
+        ...(description && { description }),
+        ...(genre && { genre }),
+        ...(director && { director }),
+        ...(year && { year }),
+        ...(rating && { rating }),
+        updatedAt: new Date(),
+      };
+
+      // const movieData = { updatetAt: new Date() };
+      // for (const [key, value] of Object.entries({
+      //   name,
+      //   description,
+      //   genre,
+      //   director,
+      //   year,
+      // })) {
+      //   if (value) {
+      //     movieData[key] = value;
+      //   }
+      // }
+
+      // const movieData = {};
+      // if (name) {
+      //   movieData.name = name
+      // }
+      // if (description) {
+      //   movieData.description = description
+      // }
+      // if (genre) {
+      //   movieData.genre = genre
+      // }
+      // if (director) {
+      //   movieData.director = director
+      // }
+      // if (year) {
+      //   movieData.year = year
+      // }
+      // movieData.updatedAt = new Date();
+
+      const success = await this.movieService.update(req.params.id, movieData);
+      if (!success) {
+        return res.status(404).json({ message: "Movie not found" });
+      }
+      res.json({ message: "Movie updated successfully" });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  async deleteMovie(req, res) {
+    try {
+      const success = await this.movieService.delete(req.params.id);
+      if (!success) {
+        return res.status(404).json({ message: "Movie not found" });
+      }
+      res.json({ message: "Movie delted successfully" });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
 }
--- G1/Class07-MongoDB/my-movies-app/services/movie.service.js
+++ G1/Class07-MongoDB/my-movies-app/services/movie.service.js
@@ -2,13 +2,35 @@ import { getDB } from "../config/db.js";
 import { ObjectId } from "mongodb";
 
 export default class MovieService {
-  getAll() {
+  async getAll() {
     return getDB().collection("myMovies").find({}).toArray();
   }
 
-  getById(id) {
+  async getById(id) {
     return getDB()
       .collection("myMovies")
       .findOne({ _id: new ObjectId(id) });
+  }
+
+  async create(movieData) {
+    const result = await getDB().collection("myMovies").insertOne(movieData);
+    // return result;
+    return result.insertedId;
+  }
+
+  async update(id, movieData) {
+    const result = await getDB()
+      .collection("myMovies")
+      .updateOne({ _id: new ObjectId(id) }, { $set: movieData });
+    return result.modifiedCount > 0;
+  }
+
+  async delete(id) {
+    const result = await getDB()
+      .collection("myMovies")
+      .deleteOne({ _id: new ObjectId(id) });
+    // return result.deletedCount > 0 ? true : false
+    // shortened way
+    return result.deletedCount > 0;
   }
 }
