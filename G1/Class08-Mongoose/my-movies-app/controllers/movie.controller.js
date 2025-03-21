fb133ed87b830b3cc8f655fc668cdc7f613d8cdb
anetastankovska <anetastankovskaane@gmail.com>
2025-03-21 18:11:13 +0100

    wip class code

--- G1/Class08-Mongoose/my-movies-app/controllers/movie.controller.js
+++ G1/Class08-Mongoose/my-movies-app/controllers/movie.controller.js
@@ -13,4 +13,45 @@ export default class MovieController {
       res.status(400).send({ message: error.message });
     }
   }
+
+  async getMovie(req, res) {
+    try {
+      const movieId = req.params.id;
+      const movie = await this.movieService.getById(movieId);
+      res.send(movie);
+    } catch (error) {
+      res.status(404).send({ message: error.message });
+    }
+  }
+
+  async createMovie(req, res) {
+    try {
+      const movieBody = req.body;
+      const movie = await this.movieService.create(movieBody);
+      res.status(201).send(movie);
+    } catch (error) {
+      res.status(400).send({ message: error.message });
+    }
+  }
+
+  async updateMovie(req, res) {
+    try {
+      const movieBody = req.body;
+      const movieId = req.params.id;
+      const movie = await this.movieService.update(movieId, movieBody);
+      res.send(movie);
+    } catch (error) {
+      res.status(400).send({ message: error.message });
+    }
+  }
+
+  async deleteMovie(req, res) {
+    try {
+      const movieId = req.params.id;
+      await this.movieService.delete(movieId);
+      res.status(200).send({ message: "Movie deleted successfully" });
+    } catch (error) {
+      res.status(400).send({ message: error.message });
+    }
+  }
 }
